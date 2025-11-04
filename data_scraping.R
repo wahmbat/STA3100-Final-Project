68312ca5d14b8abb8b1d846eb72a7c8d94318bd4
anjali <anjalitomerlin@yahoo.com>
2025-11-04 00:52:01 -0500

    added intro to report

--- data_scraping.R
+++ data_scraping.R
@@ -1,19 +1,22 @@
 #install openalexR package if not done already
 library(openalexR)
 
-#data frame of concepts of interest and their IDs
-concepts <- data.frame(concept_name = c("Mathematics","Biology","History","Computer Science","Medicine"),
-                       concept_id=c("C33923547","C86803240","C95457728","C41008148","C71924100"),
-                       counts=vector("character",5))
-
-#getting all concept's work counts for each year (going back to 2012) as a data frame and nesting in list
-counts_by_years <- list()
-
-for (i in 1:nrow(concepts)){
-  category_data <- oa_fetch(entity="concept",id=concepts[i,"concept_id"])$counts_by_year[[1]]
-  frame <- data.frame(year=category_data$year,counts = category_data$works_count)
-  counts_by_years[[length(counts_by_years)+1]] <- frame
+get_work_counts <- function(){
+  #data frame of concepts of interest and their IDs
+  concepts <- data.frame(concept_name = c("Mathematics","Biology","History","CS","Medicine","Art","Religion","Philosophy","Literature"),
+                         concept_id=c("C33923547","C86803240","C95457728","C41008148","C71924100","C142362112","C24667770","C138885662","C124952713"))
+  
+  #getting all concept's work counts for each year (going back to 2012) as a data frame and nesting in list
+  counts_by_years <- list()
+  
+  for (i in 1:nrow(concepts)){
+    category_data <- oa_fetch(entity="concepts",id=concepts[i,"concept_id"])$counts_by_year[[1]]
+    frame <- data.frame(year=category_data$year,counts = category_data$works_count) |> subset(year<=2024)
+    counts_by_years[[length(counts_by_years)+1]] <- frame
+  }
+  
+  names(counts_by_years) <- concepts$concept_name
+  
+  return(counts_by_years)
 }
-
-names(counts_by_years) <- concepts$concept_name
 
